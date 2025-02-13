91ce333fe15af2e5e22b3dd9d2ff08c996e624a3
JSJm25 <jeffreymacmullin@gmail.com>
2025-02-13 14:41:21 -0800

    added multer, wrote the post request, easy

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-var app = express();
 
+const multer = require('multer');
+const upload = multer().single('upfile');
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use((req, res, next) => {
+  let date = new Date();
+  console.log(`${req.method}, ${req.path} - ${req.ip}  at ${date.toUTCString()}`);
+  next();
+}); //Log Requests
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload, (req, res) => {
 
+  if (req.file) {
+
+    const { originalname, mimetype, size } = req.file;
+    const responseObject = {
+      name: originalname,
+      type: mimetype,
+      size
+    }
+    res.json(responseObject);
+
+  } else {
+    res.json({error: 'No file uploaded'})
+  }
+})
 
 
 const port = process.env.PORT || 3000;
